c6475d217e3a2031b21148e8aa4753fca481970e
savindu-damsara <kannangara.kdsd@gmail.com>
2026-09-15 09:51:06 +0530

    experiments

---
+++ src/models/cnn_experiment_02.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+
+
+class PlantDiseaseCNNExperiment02(nn.Module):
+    """
+    Experiment 02:
+    Baseline CNN + Batch Normalization.
+    """
+
+    def __init__(self, num_classes=38):
+        super().__init__()
+
+        # ====================================================
+        # Feature Extraction
+        # ====================================================
+
+        self.features = nn.Sequential(
+
+            # Block 1
+            nn.Conv2d(
+                in_channels=3,
+                out_channels=32,
+                kernel_size=3,
+                padding=1,
+            ),
+
+            nn.BatchNorm2d(32),
+
+            nn.ReLU(),
+
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2,
+            ),
+
+
+            # Block 2
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=3,
+                padding=1,
+            ),
+
+            nn.BatchNorm2d(64),
+
+            nn.ReLU(),
+
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2,
+            ),
+
+
+            # Block 3
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=3,
+                padding=1,
+            ),
+
+            nn.BatchNorm2d(128),
+
+            nn.ReLU(),
+
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2,
+            ),
+        )
+
+
+        # ====================================================
+        # Classification
+        # ====================================================
+
+        self.classifier = nn.Sequential(
+
+            nn.Flatten(),
+
+            nn.Linear(
+                128 * 28 * 28,
+                256,
+            ),
+
+            nn.ReLU(),
+
+            nn.Dropout(
+                p=0.5
+            ),
+
+            nn.Linear(
+                256,
+                num_classes,
+            ),
+        )
+
+
+    def forward(self, x):
+
+        x = self.features(x)
+
+        x = self.classifier(x)
+
+        return x
